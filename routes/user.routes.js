e4e8c1dfdddc243fdba593d752e656906e2c6280
ras-07 <rasswanthgpn07@gmail.com>
2026-04-10 22:46:02 +0530

    Update 1

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,7 +6,7 @@ import { authorize, checkRole } from '../middlewares/auth.middleware.js';
 
 userRouter.get('/',getUsers);
 userRouter.get('/:id',authorize,getUser);
-userRouter.put('/:id',authorize,checkRole(['Admin'],updateUser));
+userRouter.put('/:id',authorize,checkRole(['Admin']),updateUser);
 userRouter.delete('/:id',authorize,checkRole(['Admin']),deleteUser);
 
 export default userRouter;
